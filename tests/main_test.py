53e0989ae48cc37da05d46f8ea041bef9c206c24
rahi <rahi@localhost.localdomain>
2021-06-05 15:32:36 -0400

    Added Read Function & Test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,9 +1,10 @@
 from xkye import *
 import pytest
+import os
 
 #To test the missing file
 def test_read_missing_file():
-    xkyFile = "/home/rahi/Desktop/lang/xkye_python/test/test.xky"
+    xkyFile = "../test/test.xky"
     with pytest.raises(Exception):
         m = io(xkyFile)
         assert m.read() == None
@@ -11,7 +12,10 @@ def test_read_missing_file():
 
 #To test the read operation
 def test_read_file():
-    xkyFile = "/home/rahi/Desktop/lang/xkye_python/tests/test.xky"
+    xkyFile = "test.xky"
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    xkyFile = dir_path+"/"+xkyFile
 
     xkye = io(xkyFile)
     dictionary = xkye.read()
